ef1bd4ae400807614ed30af033dd7f365cc24e38
musta10 <mustafaenfadal@gmail.com>
2020-12-22 14:51:27 +0100

    api route

--- api/routes/users.js
+++ api/routes/users.js
@@ -4,6 +4,7 @@ const Mydb = require('../database');
 const bcrypt = require('bcrypt');
 const config = require('./config');
 const jwt = require('jsonwebtoken');
+const saltRounds = 10;
 
 
 // CONEXION USERS
@@ -14,7 +15,18 @@ routes.post('/sign-up', (req,res) => {
         if (!req.body.email) throw 'NO EMAIL'
         if (!req.body.password) throw 'NO PASSWORD'
         if (!req.body.photo) throw 'NO PHOTO'
-
+        bcrypt.genSalt(saltRounds, (err, salt) =>{
+            bcrypt.hash(req.body.password, salt, (err, hash) =>{
+                console.log(hash);
+                let sql = `INSERT INTO users(name, email, password, photo) VALUES ('${req.body.name}','${req.body.email}','${req.body.password}','${req.body.photo}')`;
+                Mydb.query(sql, function(err, result) {
+                    if (err) throw err;
+                    console.log(result);
+                    res.send(result)
+                })
+            })
+        })
+        
     } catch (err) {
         res.status(403).send(err)
     }
