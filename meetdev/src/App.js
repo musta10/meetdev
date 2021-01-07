82492281d0ac4c02cb8019dab1db38ef0d6073d3
musta10 <mustafaenfadal@gmail.com>
2021-01-07 13:10:54 +0100

    pageHome

--- meetdev/src/App.js
+++ meetdev/src/App.js
@@ -2,11 +2,13 @@
 // import Conexion from "./toutcomponents/Conexion"
 // import Inscription from "./toutcomponents/Inscription";
 import Nav from './component/Nav'
+import Home from './toutcomponents/Home'
 
 function App() {
   return (
     <>
     <Nav /> 
+    <Home />
     {/* <Conexion />
     <Inscription /> */}
     </>
--- meetdev/src/toutcomponents/Home.jsx
+++ meetdev/src/toutcomponents/Home.jsx
@@ -1,11 +1,48 @@
-import React from 'react';
+import React from "react";
 import "../assets/styles/home.scss";
+import Button from "react-bootstrap/Button";
+import imgEvent from "../assets/img/event1.jpg";
+import imgEvent2 from '../assets/img/event2.jfif'
 
-function Home () {
-    return (
-       <div></div>
-    )
+function Home() {
+  return (
+      <>
+    <div className="container">
+      <p>Événements a Paris, Ille De France</p>
+      <article className="fleximg">
+      <section className="portada">
+        <img src={imgEvent} alt="evenement" />
+        <div className="hover">
+          <h1 class="separ">Meet Dev</h1>
+          <p>
+            Vous Trouverez la liste des événements choisissez le meilleur
+            événement qui vous convient
+          </p>
+        </div>
+      </section>
+      <section className="portada">
+        <img src={imgEvent2} alt="evenement" />
+        <div className="hover">
+          <h1 class="separ">Meet Dev</h1>
+          <p>
+            Vous Trouverez la liste des événements choisissez le meilleur
+            événement qui vous convient
+          </p>
+        </div>
+      </section>
+      </article>
+      <div className="actualite">
+        <p>Suivez toute l'actualité des événements</p>
+        <Button className="boton-plus">En Savoir Plus</Button>
+      </div>
+    </div>
+    <div className="information">
+        
+
+    </div>
+    </>
+    
+  );
 }
 
 export default Home;
-
