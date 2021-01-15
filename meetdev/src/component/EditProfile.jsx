a82c162711320327c24fb21d5bf0a8fca38410fb
musta10 <mustafaenfadal@gmail.com>
2021-01-15 16:48:27 +0100

    pageprofile

--- meetdev/src/component/EditProfile.jsx
+++ meetdev/src/component/EditProfile.jsx
@@ -1,40 +1,41 @@
-import React from 'react'
-import Form from 'react-bootstrap/Form';
-import Button from 'react-bootstrap/Button';
-import '../assets/styles/profile.scss'
-import YourProfile from '../component/YourProfile'
+import React from "react";
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
+import "../assets/styles/profile.scss";
+import YourProfile from "../component/YourProfile";
 
 
-const Profile = ()  => {    
-    return (
-        <div className="formulario">
-        <Form>   
-  <Form.Group controlId="formBasicEmail">
-    <Form.Label>Email</Form.Label>
-    <Form.Control type="email" placeholder="email" />
-  </Form.Group>
-  <Form.Group controlId="formBasicName">
-    <Form.Label>Prénom</Form.Label>
-    <Form.Control type="text" placeholder="prénom" />
-  </Form.Group>
 
-  <Form.Group controlId="formBasicPassword">
-    <Form.Label>Mot de passe</Form.Label>
-    <Form.Control type="password" placeholder="mot de passe" />
-  </Form.Group>
+const Profile = () => {
 
-  <Form.Group controlId="formBasicText">
-    <Form.Label>Photo</Form.Label>
-    <Form.Control type="text" placeholder="Votre Photo" />
-  </Form.Group>
-  <Button variant="primary" type="submit">
-  Modifier
-  </Button>
-</Form>
-<YourProfile />
-</div>
-    )
+  return (
+    <div className="formulario">
+      <Form>
+        <Form.Group controlId="formBasicEmail">
+          <Form.Label>Email</Form.Label>
+          <Form.Control type="email" placeholder="email" />
+        </Form.Group>
+        <Form.Group controlId="formBasicName">
+          <Form.Label>Prénom</Form.Label>
+          <Form.Control type="text" placeholder="prénom" />
+        </Form.Group>
 
-}
+        <Form.Group controlId="formBasicPassword">
+          <Form.Label>Mot de passe</Form.Label>
+          <Form.Control type="password" placeholder="mot de passe" />
+        </Form.Group>
+
+        <Form.Group controlId="formBasicText">
+          <Form.Label>Photo</Form.Label>
+          <Form.Control type="text" placeholder="Votre Photo" />
+        </Form.Group>
+        <Button variant="primary" type="submit">
+          Modifier
+        </Button>
+      </Form>
+      <YourProfile />
+    </div>
+  );
+};
+
 export default Profile;
-
--- meetdev/src/component/YourProfile.jsx
+++ meetdev/src/component/YourProfile.jsx
@@ -2,19 +2,29 @@ import React from 'react'
 import ListGroup from 'react-bootstrap/ListGroup'
 import Card from 'react-bootstrap/Card'
 import imageUser from '../assets/img/event2.jfif'
+import Button from 'react-bootstrap/Button';
+import { useHistory } from "react-router-dom";
+
 
 const userProfile = () => {
+  let history = useHistory();
+
+  function Homeclick() {
+    history.push("/home");
+  }
+  
     return (
-    
         <Card style={{ width: '18rem' }}>
         <h3>Votre Profile</h3>
-        <Card.Header> <img style={{width:80, height:80, borderRadius: '50%'}} src={imageUser} alt=""/> </Card.Header>
+        <Card.Header className="logos"> <img style={{width:80, height:80, borderRadius: '50%'}} src={imageUser} alt=""/></Card.Header>
         <ListGroup variant="flush">
           <ListGroup.Item>Prénom</ListGroup.Item>
           <ListGroup.Item>Email</ListGroup.Item>
           <ListGroup.Item>Mot de passe</ListGroup.Item>
+          <Button  onClick={Homeclick} className="btn-block bouton-login">Page Accueil</Button>
         </ListGroup>
       </Card>
+        
     )
 
 }
