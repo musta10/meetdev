098a5ccdaceda3fdeba401506bcc10deb0d54a48
musta10 <mustafaenfadal@gmail.com>
2021-01-15 12:42:07 +0100

    message error

--- meetdev/src/component/YourProfile.jsx
+++ meetdev/src/component/YourProfile.jsx
@@ -1,14 +1,14 @@
 import React from 'react'
 import ListGroup from 'react-bootstrap/ListGroup'
 import Card from 'react-bootstrap/Card'
-import imageUser from '../assets/img/event1.jpg'
+import imageUser from '../assets/img/event2.jfif'
 
 const userProfile = () => {
     return (
     
         <Card style={{ width: '18rem' }}>
         <h3>Votre Profile</h3>
-        <Card.Header> <img style={{width:50, height:50, borderRadius: '50%'}} src={imageUser} alt=""/> </Card.Header>
+        <Card.Header> <img style={{width:80, height:80, borderRadius: '50%'}} src={imageUser} alt=""/> </Card.Header>
         <ListGroup variant="flush">
           <ListGroup.Item>Prénom</ListGroup.Item>
           <ListGroup.Item>Email</ListGroup.Item>
