de833f3eb1a2b1d9669fe78ae19c5f62e4950704
musta10 <mustafaenfadal@gmail.com>
2021-02-22 17:57:11 +0100

    eventslist

--- meetdev/src/toutcomponents/EventsList.jsx
+++ meetdev/src/toutcomponents/EventsList.jsx
@@ -1,25 +1,25 @@
-import React from "react";
-import Nav from "../component/Nav";
-import "../assets/styles/events.scss";
-import { useSelector } from "react-redux";
-import { AiFillDelete } from "react-icons/ai";
-import { AiFillEdit } from "react-icons/ai";
-import { AiOutlineCalendar } from "react-icons/ai";
+import React from 'react'
+import Nav from '../component/Nav'
+import '../assets/styles/events.scss'
+import {useSelector} from 'react-redux'
+import { FaRegCommentAlt } from "react-icons/fa";
+import { FaRegCalendarMinus } from "react-icons/fa";
 import Moment from "react-moment";
 
 const Events = () => {
-  const events = useSelector((state) => state.eventsReducer.events);
-  console.log(events);
-  //// ICI LA LISTE EVENTS .MAP
 
-  return (
+    const events = useSelector(state => state.eventsReducer.events )
+    console.log(events)
+//// ICI LA LISTE EVENTS .MAP
+
+
+
+return(
     <>
-      <Nav />
-      <p style={{ marginTop: 20, textAlign: "center" }}>
-        Événements a Paris, Ille De France
-      </p>
-      <div>
-        {events.map((elem, index) => {
+    <Nav />
+    <p style={{marginTop: 20, textAlign:'center'}}>Événements a Paris, Ille De France</p>
+    <div>
+    {events.map((elem, index) => {
           return (
             <article key={index} className="card_evenement">
               <div className="date">
@@ -30,20 +30,23 @@ const Events = () => {
                 <p>{elem.description}</p>
               </div>
               <div className="edit-icons">
-                <AiOutlineCalendar
+             <FaRegCalendarMinus
+              className="separ"
+              color="#000000"
+              size={30} />
+                <FaRegCommentAlt
                   className="separ"
                   color="#000000"
-                  size={40}
+                  size={30}
                 />
-                <AiFillEdit className="separ" color="#000000" size={40} />
-                <AiFillDelete color="#000000" size={40} />
+                <p>commenter</p>
               </div>
             </article>
           );
         })}
-      </div>
+    </div>
     </>
-  );
-};
+)
+}
 
 export default Events;
