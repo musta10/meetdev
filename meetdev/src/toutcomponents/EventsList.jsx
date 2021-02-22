6e885e4be6ff0c2265b9dc307c7565114029e8c1
musta10 <mustafaenfadal@gmail.com>
2021-02-22 17:08:49 +0100

    listesevents

--- meetdev/src/toutcomponents/EventsList.jsx
+++ meetdev/src/toutcomponents/EventsList.jsx
@@ -2,6 +2,9 @@ import React from 'react'
 import Nav from '../component/Nav'
 import '../assets/styles/events.scss'
 import {useSelector} from 'react-redux'
+import { AiFillDelete } from "react-icons/ai";
+import { AiFillEdit } from "react-icons/ai";
+import Moment from "react-moment";
 
 const Events = () => {
 
@@ -15,6 +18,29 @@ return(
     <>
     <Nav />
     <p style={{marginTop: 20, textAlign:'center'}}>Événements a Paris, Ille De France</p>
+    <div>
+    {events.map((elem, index) => {
+          return (
+            <article key={index} className="card_evenement">
+              <div className="date">
+                <Moment style={{ color: "#d63031" }} format="YYYY/MM/DD">
+                  {elem.date}
+                </Moment>
+
+                <p>{elem.description}</p>
+              </div>
+              <div className="edit-icons">
+                <AiFillEdit
+                  className="separ"
+                  color="#0984e3"
+                  size={40}
+                />
+                <AiFillDelete color="#808080" size={40} />
+              </div>
+            </article>
+          );
+        })}
+    </div>
     </>
 )
 }
