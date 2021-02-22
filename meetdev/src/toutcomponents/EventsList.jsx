5d3f58553c6c6ae1e3ab4f7dbc9903a906342b45
musta10 <mustafaenfadal@gmail.com>
2021-02-22 17:45:05 +0100

    icons

--- meetdev/src/toutcomponents/EventsList.jsx
+++ meetdev/src/toutcomponents/EventsList.jsx
@@ -1,25 +1,25 @@
-import React from 'react'
-import Nav from '../component/Nav'
-import '../assets/styles/events.scss'
-import {useSelector} from 'react-redux'
+import React from "react";
+import Nav from "../component/Nav";
+import "../assets/styles/events.scss";
+import { useSelector } from "react-redux";
 import { AiFillDelete } from "react-icons/ai";
 import { AiFillEdit } from "react-icons/ai";
+import { AiOutlineCalendar } from "react-icons/ai";
 import Moment from "react-moment";
 
 const Events = () => {
+  const events = useSelector((state) => state.eventsReducer.events);
+  console.log(events);
+  //// ICI LA LISTE EVENTS .MAP
 
-    const events = useSelector(state => state.eventsReducer.events )
-    console.log(events)
-//// ICI LA LISTE EVENTS .MAP
-
-
-
-return(
+  return (
     <>
-    <Nav />
-    <p style={{marginTop: 20, textAlign:'center'}}>Événements a Paris, Ille De France</p>
-    <div>
-    {events.map((elem, index) => {
+      <Nav />
+      <p style={{ marginTop: 20, textAlign: "center" }}>
+        Événements a Paris, Ille De France
+      </p>
+      <div>
+        {events.map((elem, index) => {
           return (
             <article key={index} className="card_evenement">
               <div className="date">
@@ -30,19 +30,20 @@ return(
                 <p>{elem.description}</p>
               </div>
               <div className="edit-icons">
-                <AiFillEdit
+                <AiOutlineCalendar
                   className="separ"
                   color="#000000"
                   size={40}
                 />
+                <AiFillEdit className="separ" color="#000000" size={40} />
                 <AiFillDelete color="#000000" size={40} />
               </div>
             </article>
           );
         })}
-    </div>
+      </div>
     </>
-)
-}
+  );
+};
 
 export default Events;
